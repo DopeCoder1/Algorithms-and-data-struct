0321e22f0c338e1965cd14a15969c8f6d67d9e6f
trippyCoder313 <58041343+trippyCoder313@users.noreply.github.com>
2020-09-15 06:13:14 +0600

    oop base

--- 1)Intro to opp.cpp
+++ 1)Intro to opp.cpp
@@ -1,13 +1,49 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+class Print;
+
+class Student{
+private://Приватная только для класса!
+  string name;
+  string surname;
+  int age;
+  int gpa;
+
+public://Внешняя func()
+  friend Print;
+  //getter
+  void gets() const
+  {
+    cout<<name<<" "<<surname<<" "<<age<<" "<<gpa<<endl;
+  }
+
+  void sets(string name_,string surname_,int age_,int gpa_)
+  {
+    name=name_;
+    surname=surname_;
+    age=age_;
+    gpa=gpa_;
+
+  }
+};
+
+class Print{
+public:
+  void printStudent(Student s)
+  {
+  cout<<s.name<<" ";
+  }
+};
+
 int main()
 {
-  
-  int number=30;
-  int *n=&number;
-  cout<<"adress : "<<n<<endl;
-  cout<<"hello world"<<endl;
+  Print p;
+  Student s;
+  s.sets("dias","alimbayev",18,2);
+  p.printStudent(s);
+
   return 0;
 }
