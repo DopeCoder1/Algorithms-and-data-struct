b4eeb688984075c130bb6c597768651d61cdb821
trippyCoder313 <58041343+trippyCoder313@users.noreply.github.com>
2020-09-16 12:14:03 +0600

    succeful

--- lab/2)abiturent.cpp
+++ lab/2)abiturent.cpp
@@ -65,13 +65,92 @@ public:
   }
 
 
-  friend void show(Abiturent sv);
 
+  friend void show(int n,Abiturent *ls);
+  friend void ls_normal(int n,Abiturent *ls1);
+  friend void ls_notpass(int n,Abiturent *ls2);
+  friend void ls_n(int n,Abiturent *ls3);
+  friend void max(int n,Abiturent *ls4);
 };
 
-void show(Abiturent sv)
+void max(int n,Abiturent *ls4)
 {
-  cout<< sv.name <<" "<< sv.surname <<" "<< sv.fname <<" "<< sv.adress <<" "<< sv.gpa <<endl;
+  double mx=ls4[0].get_gpa();
+  for(int j=0;j<n;j++)
+  {
+    if(ls4[j].get_gpa()>mx)
+    {
+      mx=ls4[j].get_gpa();
+    }
+  }
+
+  for(int j=0;j<n;j++)
+  {
+    if(ls4[j].get_gpa()==mx)
+    {
+      cout<<"---------------"<<endl;
+      cout<<"max score: ";
+      cout<< ls4[j].surname <<" "<< ls4[j].name <<" "<< ls4[j].fname <<" "<< ls4[j].adress <<" "<< ls4[j].gpa <<endl;
+      cout<<"---------------"<<endl;
+    }
+  }
+
+}
+
+void show(int n,Abiturent *ls)
+{
+  cout<<"all list: "<<endl;
+
+  for(int j=0;j<n;j++)
+  {
+    cout<<"---------------"<<endl;
+    cout<< ls[j].surname <<" "<< ls[j].name <<" "<< ls[j].fname <<" "<< ls[j].adress <<" "<< ls[j].gpa <<endl;
+    cout<<"---------------"<<endl;
+  }
+}
+
+void ls_normal(int n,Abiturent *ls1)
+{
+  for(int j=0;j<n;j++)
+  {
+    if(ls1[j].get_gpa()>2)
+    {
+      cout<<"---------------"<<endl;
+      cout<<"pass :";
+      cout<< ls1[j].surname <<" "<< ls1[j].name <<" "<< ls1[j].fname <<" "<< ls1[j].adress <<" "<< ls1[j].gpa <<endl;
+      cout<<"---------------"<<endl;
+
+    }
+  }
+}
+
+void ls_notpass(int n,Abiturent *ls2)
+{
+  for(int j=0;j<n;j++)
+  {
+  if(ls2[j].get_gpa()<2)
+  {
+   cout<<"---------------"<<endl;
+   cout<<" didn't pass :";
+    cout<< ls2[j].surname <<" "<< ls2[j].name <<" "<< ls2[j].fname <<" "<< ls2[j].adress <<" "<< ls2[j].gpa <<endl;
+    cout<<"---------------"<<endl;
+
+  }
+  }
+}
+
+void ls_n(int n,Abiturent *ls3)
+{
+  for(int j=0;j<n;j++)
+  {
+    if(ls3[j].get_gpa()>=2 && ls3[j].get_gpa()<2.1)
+    {
+      cout<<"---------------"<<endl;
+      cout<<" maybe pass :";
+       cout<< ls3[j].surname <<" "<< ls3[j].name <<" "<< ls3[j].fname <<" "<< ls3[j].adress <<" "<< ls3[j].gpa <<endl;
+       cout<<"---------------"<<endl;
+    }
+  }
 }
 
 
@@ -79,16 +158,15 @@ void show(Abiturent sv)
 int main()
 {
   Abiturent *s;
-  int *ss;
   int n;
   cin>>n;
-  ss=new int[n];
   s=new Abiturent[n];
   string name,surname,fname,adress;
   double gpa;
+
   for(int j=0;j<n;j++)
   {
-    cin>> name >> surname >> fname >> adress >> gpa;
+    cin>> surname >> name >> fname >> adress >> gpa;
     s[j].set_name(name);
     s[j].set_surname(surname);
     s[j].set_fname(fname);
@@ -96,37 +174,12 @@ int main()
     s[j].set_gpa(gpa);
   }
 
-  int max=s[0].get_gpa();
 
-
-  for(int j=0;j<n;j++)
-  {
-    if(s[j].get_gpa()>max)
-    {
-      max=s[j].get_gpa();
-    }
-  }
-  cout<<"---------------"<<endl;
-  cout<<"record result is: "<<max<<endl;
-  cout<<"---------------"<<endl;
-
-  for(int j=0;j<n;j++)
-  {
-    if(s[j].get_gpa()<2)
-    {
-      cout<<"didn't pass: ";
-      show(s[j]);
-      cout<<"---------------"<<endl;
-    }
-
-    else if(s[j].get_gpa()>2)
-    {
-      cout<<"pass: ";
-      show(s[j]);
-      cout<<"---------------"<<endl;
-    }
-  }
-
-
+  ls_notpass(n,s);
+  ls_normal(n,s);
+  max(n,s);
+  ls_n(n,s);
+  
+  delete[] s;
   return 0;
 }
